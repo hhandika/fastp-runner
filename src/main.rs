9035ec3295b74c1ea3cbec00e388fe5b4f8ce13b
Heru Handika <herubiolog@gmail.com>
2021-02-20 21:33:09 -0600

    Add execution time and greetings.

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,18 @@ mod io;
 mod parser;
 mod wrapper;
 
+use std::time::Instant;
+
 use clap::crate_version;
 
 fn main() {
     let version = crate_version!();
+    
+    let time = Instant::now();
     println!("Starting fastp-runner v{}", &version);
     cli::get_cli(&version);
+    let duration = time.elapsed();
 
+    println!("Execution time: {:?}", duration);
+    println!("Thank you for using fastp-runner v{}", &version);
 }
