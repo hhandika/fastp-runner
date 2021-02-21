fcf2467e6ea64c5afc2e767db80d9d1d0fda45c0
Heru Handika <herubiolog@gmail.com>
2021-02-20 21:40:13 -0600

    Colorize output and add an empty line before printing results.

--- src/io.rs
+++ src/io.rs
@@ -5,9 +5,10 @@ use crate::parser::{self, RawSeq};
 pub fn dry_run(input: &PathBuf) {
     let seq_reads: Vec<RawSeq> = parser::parse_csv(input, true);
 
+    println!();
     seq_reads.iter()
         .for_each(|reads| {
-            println!("ID\t: {}", reads.id);
+            println!("\x1b[0;32mID\t: {}\x1b[0m", reads.id);
             println!("Dir\t: {}", reads.dir.to_string_lossy());
             println!("Read 1\t: {}", reads.read_1.to_string_lossy());
             println!("Read 2\t: {}", reads.read_2.to_string_lossy());
