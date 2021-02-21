f9f79fa6b7de8f605b2fd0b0d15d1c9324183f67
Heru Handika <herubiolog@gmail.com>
2021-02-20 21:34:42 -0600

    Add emoji.

--- src/main.rs
+++ src/main.rs
@@ -20,5 +20,5 @@ fn main() {
     let duration = time.elapsed();
 
     println!("Execution time: {:?}", duration);
-    println!("Thank you for using fastp-runner v{}", &version);
+    println!("Thank you for using fastp-runner v{}  😊", &version);
 }
