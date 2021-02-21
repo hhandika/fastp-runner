c8fccab00a764865d5f2f89a36791fc7844cd1a5
Heru Handika <herubiolog@gmail.com>
2021-02-21 13:04:18 -0600

    Print i7 index in the console.

--- src/io.rs
+++ src/io.rs
@@ -8,11 +8,19 @@ pub fn dry_run(input: &PathBuf) {
     println!();
     seq_reads.iter()
         .for_each(|reads| {
-            println!("\x1b[0;32mID\t: {}\x1b[0m", reads.id);
-            println!("Dir\t: {}", reads.dir.to_string_lossy());
-            println!("Read 1\t: {}", reads.read_1.to_string_lossy());
-            println!("Read 2\t: {}", reads.read_2.to_string_lossy());
-            println!("Adapter\t: {}", reads.adapter_i5);
+            println!("\x1b[0;32mID\t\t: {}\x1b[0m", reads.id);
+            println!("Dir\t\t: {}", reads.dir.to_string_lossy());
+            println!("Read 1\t\t: {}", reads.read_1.to_string_lossy());
+            println!("Read 2\t\t: {}", reads.read_2.to_string_lossy());
+
+            match reads.adapter_i7.as_ref() {
+                Some(i7) => {
+                    println!("Adapter i5\t: {}", reads.adapter_i5);
+                    println!("Adapter i7\t: {}", i7);
+                }
+                None => println!("Adapter\t\t: {}", reads.adapter_i5),
+            };
+
             println!();
         });
 }
