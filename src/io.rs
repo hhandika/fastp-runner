6cacae34f32489c78dfd8c4ddc12c896ff83bca7
Heru Handika <herubiolog@gmail.com>
2021-02-23 13:52:04 -0600

    Move fastp check before parsing csv.

--- src/io.rs
+++ src/io.rs
@@ -35,9 +35,8 @@ pub fn dry_run(input: &PathBuf) {
 
 pub fn process_input(input: &PathBuf, is_mid_id: bool, version: &str) {
     println!("Starting fastp-runner v{}...", &version);
-
-    let reads: Vec<RawSeq> = parser::parse_csv(input, is_mid_id);
     display_fastp_status();
+    let reads: Vec<RawSeq> = parser::parse_csv(input, is_mid_id);
     wrapper::clean_reads(&reads);
 }
 
