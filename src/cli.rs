8e6073048593878e8fe95fe3ff1d9de0b35988e5
Heru Handika <herubiolog@gmail.com>
2021-02-26 19:46:34 -0600

    Refactor cli and add test for it.

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use clap::{App, AppSettings, Arg};
+use clap::{App, AppSettings, Arg, ArgMatches};
 
 use crate::io;
 use crate::wrapper;
@@ -30,8 +30,8 @@ pub fn get_cli(version: &str) {
                 )
                 
                 .arg(
-                    Arg::with_name("left_id")
-                        .long("left")
+                    Arg::with_name("id")
+                        .long("id")
                         .help("A unique ID is at the start of filename")
                         .takes_value(false)
                 )
@@ -47,30 +47,70 @@ pub fn get_cli(version: &str) {
         .get_matches();
 
     match args.subcommand() {
-        ("clean", Some(clean_matches)) => {
-
-            if clean_matches.is_present("input") {
-                let path = PathBuf::from(clean_matches.value_of("input").unwrap());
-                let mut is_mid_id = true;
-
-                if clean_matches.is_present("left_id") {
-                    is_mid_id = false;
-                }
-
-                if clean_matches.is_present("dry-run") {
-                    io::dry_run(&path);
-                } else {
-                    io::process_input(&path, is_mid_id, version);
-                }
-
-            }  
-
-        }
-
-        ("check", Some(_)) => {
-            wrapper::check_fastp();
-        }
-
+        ("clean", Some(clean_matches)) => run_fastp_clean(clean_matches, version),
+        ("check", Some(_)) => wrapper::check_fastp(),
         _ => unreachable!("UNREACHABLE COMMANDS!"),
     };
+}
+
+fn run_fastp_clean(clean_matches: &ArgMatches, version: &str) {
+    if clean_matches.is_present("input") {
+        let path = PathBuf::from(clean_matches.value_of("input").unwrap());
+        let mut is_mid_id = true;
+
+        if clean_matches.is_present("id") {
+            is_mid_id = false;
+        }
+
+        if clean_matches.is_present("dry-run") {
+            io::dry_run(&path);
+        } else {
+            io::process_input(&path, is_mid_id, version);
+        }
+
+    } 
+}
+
+#[cfg(test)]
+mod test {
+    use assert_cmd::Command;
+    use clap::crate_version;
+    use predicates::prelude::*;
+
+    #[test]
+    fn cli_general_test() {
+        let mut cmd = Command::cargo_bin("ftr").unwrap();
+        cmd.arg("--help");
+        cmd.assert().success();
+    }
+
+    #[test]
+    fn cli_version_test() {
+        let version = crate_version!();
+        let mut cmd = Command::cargo_bin("ftr").unwrap();
+        cmd.arg("--version");
+        cmd.assert()
+            .success()
+            .stdout(predicate::str::contains(version));
+    }
+
+    #[test]
+    fn cli_clean_test() {
+        let mut cmd = Command::cargo_bin("ftr").unwrap();
+        cmd.arg("clean").arg("--help");
+        cmd.assert().success();
+    }
+
+    #[test]
+    fn cli_clean_invalid_file_test() {
+        let mut cmd = Command::cargo_bin("ftr").unwrap();
+
+        cmd.arg("clean")
+            .arg("-i")
+            .arg("invalid.txt");
+        
+        cmd.assert()
+            .failure()
+            .stderr(predicate::str::contains("No such file or directory"));
+    }
 }
