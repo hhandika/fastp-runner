a42a946c5a29b7965e14422102dcdf7ef70da246
Heru Handika <herubiolog@gmail.com>
2021-02-28 09:15:25 -0600

    Print target name to console for dry run.

--- src/io.rs
+++ src/io.rs
@@ -1,4 +1,5 @@
 use std::path::PathBuf;
+use std::io::{self, Write};
 
 use crate::parser::{self, RawSeq};
 use crate::wrapper;
@@ -6,30 +7,39 @@ use crate::wrapper;
 pub fn dry_run(input: &PathBuf, is_id: bool, is_rename: bool) {
     display_fastp_status();
     let reads: Vec<RawSeq> = parser::parse_csv(input, is_id, is_rename);
+    let stdout = io::stdout();
+    let mut handle = io::BufWriter::new(stdout);
 
     println!();
     reads.iter()
         .for_each(|r| {
-            println!("\x1b[0;32mID\t\t: {}\x1b[0m", r.id);
-            println!("Dir\t\t: {}", r.dir.to_string_lossy());
-            println!("Read 1\t\t: {}", r.read_1.to_string_lossy());
-            println!("Read 2\t\t: {}", r.read_2.to_string_lossy());
+            writeln!(handle, "\x1b[0;32mID\t\t: {}\x1b[0m", r.id).unwrap();
+            writeln!(handle, "Dir\t\t: {}", r.dir.to_string_lossy()).unwrap();
+            writeln!(handle, "Read 1\t\t: {}", r.read_1.to_string_lossy()).unwrap();
+            writeln!(handle, "Read 2\t\t: {}", r.read_2.to_string_lossy()).unwrap();
 
             match r.adapter_i7.as_ref() {
                 Some(i7) => {
-                    println!("Adapter i5\t: {}", r.adapter_i5.as_ref().unwrap());
-                    println!("Adapter i7\t: {}", i7);
+                    writeln!(handle, "Adapter i5\t: {}", 
+                        r.adapter_i5.as_ref().unwrap()).unwrap();
+                    writeln!(handle, "Adapter i7\t: {}", i7).unwrap();
                 }
                 None => {
                     if r.auto_idx {
-                        println!("Adapter\t\t: AUTO-DETECT");
+                        writeln!(handle, "Adapter\t\t: AUTO-DETECT").unwrap();
                     } else {
-                        println!("Adapter\t\t: {}", r.adapter_i5.as_ref().unwrap());
+                        writeln!(handle, "Adapter\t\t: {}", 
+                            r.adapter_i5.as_ref().unwrap()).unwrap();
                     }
                 }
             };
 
-            println!();
+            if is_rename {
+                writeln!(handle, "Target fname\t: {}", 
+                    r.outname.as_ref().unwrap().to_string_lossy()).unwrap() 
+            }
+
+            writeln!(handle).unwrap();
         });
 
 }
